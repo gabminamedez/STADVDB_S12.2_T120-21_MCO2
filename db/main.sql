61c8a261ed37f5ff8e3eee841f4f340abed8922a
gabminamedez <gabminamedez@gmail.com>
2021-01-25 10:59:21 +0800

    Renamed sql to db folder; Added initial sql file to be edited

---
+++ db/main.sql
@@ -0,0 +1,8 @@
+USE dpuui2kh95t8cclk;
+
+DROP TABLE IF EXISTS neighbourhood_groups;
+DROP TABLE IF EXISTS neighbourhoods;
+DROP TABLE IF EXISTS locations;
+DROP TABLE IF EXISTS hosts;
+DROP TABLE IF EXISTS room_types;
+DROP TABLE IF EXISTS listings;
