8d35e291ea96e26fddd627995bfdb7842fe076fd
gabminamedez <gabminamedez@gmail.com>
2021-01-26 16:42:47 +0800

    Finished ETL

--- db/main.sql
+++ db/main.sql
@@ -1,11 +1,11 @@
 USE dpuui2kh95t8cclk;
 
-DROP TABLE IF EXISTS neighbourhood_groups;
-DROP TABLE IF EXISTS neighbourhoods;
-DROP TABLE IF EXISTS locations;
+DROP TABLE IF EXISTS listings;
 DROP TABLE IF EXISTS hosts;
 DROP TABLE IF EXISTS room_types;
-DROP TABLE IF EXISTS listings;
+DROP TABLE IF EXISTS locations;
+DROP TABLE IF EXISTS neighbourhoods;
+DROP TABLE IF EXISTS neighbourhood_groups;
 
 CREATE TABLE neighbourhood_groups (
 	neighbourhood_group_id INT AUTO_INCREMENT,
